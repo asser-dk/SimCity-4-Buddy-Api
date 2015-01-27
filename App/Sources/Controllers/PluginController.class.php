0ae62159b2d452ae0ddeb1522d93c2e4b56adcc4
Asbjørn Hansen <aha@trustpilot.com>
2015-01-27 17:37:20 +0100

    added getplugin member to plugincontroller

--- App/Sources/Controllers/PluginController.class.php
+++ App/Sources/Controllers/PluginController.class.php
@@ -3,11 +3,54 @@ class PluginController implements IController
 {
     public function RouteTable()
     {
-        return array();
+        return array(
+            'plugin' => array(
+                'methods' => array(
+                    'GET' => array('method' => 'GetPlugin')
+                ),
+                'controller' => $this,
+                'documentation' => array('/plugins/{pluginId]' => 'Get info on a specific plugin'),
+                'regex' => '/^\/plugins\/[A-z0-9-]{36}$/',
+                'arguments' => array(
+                    'pluginId' => array(
+                        'pattern' => '/^\/plugins\/([A-z0-9-]{36})$/',
+                        'index' => 1
+                    )
+                )
+            )
+        );
     }
 
     public function ProcessRequest(string $memberName, array $arguments = null)
     {
+        switch($memberName)
+        {
+            case 'GetPlugin':
+                return $this->GetPlugin($arguments['pluginId']);
+            default:
+                throw new NotFoundException(GeneralError::ResourceNotFound, 'The requested resource was not found on this server.');
+        }
+    }
+
+    public function GetPlugin(string $pluginId)
+    {
+        if(!Guid::IsValid($pluginId))
+        {
+            throw new BadRequestException(GeneralError::MalformedId, 'Plugin id is malformed.');
+        }
+
+        $plugin = $this->Register->GetPlugin($pluginId);
+
+        if($plugin === null)
+        {
+            header('HTTP/1.1 404 Not Found');
+        }
+        else
+        {
+            header('HTTP/1.1 200 OK');
+        }
+
+        return $plugin;
     }
 }
 ?>
