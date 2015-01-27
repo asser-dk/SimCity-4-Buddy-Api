34d10f7edae6e1981b6b828a9e98a2ea13d45f46
Asbjørn Hansen <aha@trustpilot.com>
2015-01-27 17:30:04 +0100

    added new controller for plugins

---
+++ App/Sources/Controllers/PluginController.class.php
@@ -0,0 +1,13 @@
+<?php
+class PluginController implements IController
+{
+    public function RouteTable()
+    {
+        return array();
+    }
+
+    public function ProcessRequest(string $memberName, array $arguments = null)
+    {
+    }
+}
+?>
