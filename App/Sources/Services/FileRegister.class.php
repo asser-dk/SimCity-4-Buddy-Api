50c066038c2e9198f93d3b927e33b7a7f35c40d8
Asbjørn Hansen <aha@trustpilot.com>
2015-01-27 17:07:10 +0100

    added file class and method to fileregister to get files

--- App/Sources/Services/FileRegister.class.php
+++ App/Sources/Services/FileRegister.class.php
@@ -2,10 +2,45 @@
 class FileRegister
 {
     private $MySql;
-    
+
     public function __construct(mysqli $mySqli)
     {
         $this->MySql = $mySqli;
+    }
+
+    public function GetAllFiles(integer $page, integer $perPage, string $orderBy)
+    {
+        $offset = PaginationHelper::CalculateOffset($page, $perPage);
+
+        $statementString = '
+            SELECT
+                `File`.`Id` AS `id`,
+                `File`.`Checksum` AS `checksum`,
+                `File`.`Filename` AS `filename`,
+                `File`.`Plugin` AS `pluginId`
+            FROM `File`
+            ORDER BY ' . $orderBy . '
+            LIMIT ?, ?';
+
+        $statement = $this->MySql->prepare($statementString);
+        $statement->bind_param('ii', $offset, $perPage);
+        $statement->execute();
+        $statement->bind_result($id, $checksum, $filename, $pluginId);
+
+        $files = array();
+
+        while($statement->fetch())
+        {
+            $file = new File();
+            $file->Id = $id;
+            $file->Checksum = $checksum;
+            $file->Filename = $filename;
+            $file->Plugin = $pluginId;
+        }
+
+        $statement->close();
+
+        return $files;
     }
 }
 ?>
